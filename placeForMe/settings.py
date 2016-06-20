b3a9836ba2cb88cf31574f202be59d81bb9ccb4f
Joseba <jcarraldc@gmail.com>
2016-06-20 22:28:40 +0200

    Deploy en heroku

--- placeForMe/settings.py
+++ placeForMe/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
